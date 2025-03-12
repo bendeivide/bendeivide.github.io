d716952f9933bde686e7a6b05c03de22dbff6346
bendeivide <ben.deivide@gmail.com>
2025-03-12 08:10:35 -0300

    aprendendoleem.R

--- docs/js/wowchemy-slides.js
+++ docs/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { font: "minimal", highlight_style: "dracula", theme: "moon" };
 
-  // ns-hugo:/media/ben10/Backup/BEN_PROJ_GIT_11.03.25/bendeivide.github.io(11.03.2025)/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
+  // ns-hugo:/media/ben10/Backup/BEN_PROJ_GIT_12.03.25/bendeivide.github.io(12.03.2025)/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
--- static/aprendendoleem/aprendleem.R
+++ static/aprendendoleem/aprendleem.R
@@ -27,17 +27,27 @@ d2 <- d2$challenger
 #####################
 
 ## VQ discreta
-#...
+d1 |>
+  new_leem() |>
+  tabfreq()
 
 ## VQ continua
-#...
+res <- d2 |>
+  new_leem(variable = 2) |>
+  tabfreq()
 
 ## Variavel qualitativa
 ### Exemplo 3 - funcao tabfreq()
 ### Exemplo 2 - funcao stickchart() 
+school <- rep(c("high", "university", "basic"), 3:5)
+sample(school, 30, TRUE) |>
+  new_leem() |>
+  tabfreq(ordered = c("basic", "high", "university"))
+  
+
 
 ## Detalhar com a tabulacao foi realizada
-#...
+
 
 ##########
 # Graficos
@@ -53,16 +63,29 @@ d2 <- d2$challenger
 #...
 
 # Grafico de histograma
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  hist()
 
 # Grafico de ogiva
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  ogive(freq = "p", decreasing = TRUE, bars = TRUE)
 
 # Poligono de frequencia
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  polyfreq()
 
 # Grafico Boxplot
-#...
+showboxplot()
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  boxplot(type = "classes")
 
 # Grafico de pizza
 #...
@@ -73,37 +96,73 @@ d2 <- d2$challenger
 
 ## Media
 ### Agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  mean()
+
 ### Nao agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  mean(grouped = FALSE)
 
 ## Mediana
 ### Agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  median()
 ### Nao agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  median(grouped = FALSE)
 
 ## Moda
 ### Agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  mfreq()
 ### Nao agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  mfreq(grouped = FALSE)
 
 ## 3 medidas juntas
 ### Agrupado
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  mpos()
 ### Nao agrupado
 #...
 
 ## Medidas representadas em graficos
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  hist() |>
+  insert(type = "all")
+
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  ogive(both = TRUE) |>
+  insert(type = "median")
+
 
 ######################
 # Medidas de dispersao
 ######################
 
 # Amplitude
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  amplitude()
 
 # Modulo do desvio medio
 # madev()
@@ -112,16 +171,28 @@ d2 <- d2$challenger
 # medev()
 
 # Variancia
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  variance()
 
 # Desvio padrao
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  sdev()
 
 # Coeficiente de Variacao
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  cv()
 
 # Erro padrao da media
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  mstde()
 
 ######################
 # Assimetria e Curtose
@@ -130,7 +201,10 @@ d2 <- d2$challenger
 ## Assimetria (Mostrar)
 showskew()
 ## Calcular
-#...
+d2 |>
+  new_leem(variable = 2) |>
+  tabfreq() |>
+  skewness()
 
 ## Curtose (Mostrar)
 showkur()
@@ -142,7 +216,9 @@ showkur()
 ################################
 
 # Propriedades da Funcao de distribuicao
-showcdf()
+showcdf(prop = 1) # prop = 1, 2, 3
+showcdf(variable = "continuous" , prop = 3)
+
 
 # VA Discreta
 ## Funcao de probabilidade (Qualquer)
@@ -155,14 +231,15 @@ fda <- c(0.23, 0.5, 0.8, 0.92, 1)
 cdfd(x, fda)
 
 ## VAs discreta e continua (Funcao P())
-#...
+P(-1 %>x>% 1, dist = "normal", mean = 0, sd = 1,
+  lower.tail = FALSE)
 
 ## Funcao Quantil (Funcao Q())
-#...
+
 
 ## Propriedades da distribuicao normal
 ### type = 1, 2, 3, 4, 5
-propofnormal()
+propofnormal(type = 5)
 
 ## Entendendo o computo de probabilidade
 #   de uma distribuicao normal
